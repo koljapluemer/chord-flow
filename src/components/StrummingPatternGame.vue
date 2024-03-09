9b1fa7676ce88838b44ab9ba9c8cbc85a16c8349
blackbrokkoli <kastomat@gmail.com>
2024-03-09 15:47:01 +0100

    implement simple difficulty selector for strumming patterns

--- src/components/StrummingPatternGame.vue
+++ src/components/StrummingPatternGame.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from "vue";
+import { ref, watch, onMounted } from "vue";
 import { useRouter } from "vue-router";
 import StopButton from "./StopButton.vue";
 
@@ -13,45 +13,82 @@ const easyChords = [
   { name: "C7", frets: "0001" },
 ];
 
-const strummingPatterns = [
+const defaultStrummingPatternData = [
   {
     name: "Island Strum",
     moves: "↓-↓↑-↑↓↑",
+    difficulty: 0.6,
   },
   {
     name: "Simple X",
     moves: "↓-↑-x-↑-",
+    difficulty: 0.4,
   },
   {
     name: "UpDownUpDown Forever",
     moves: "↓↑↓↑↓↑↓↑",
+    difficulty: -0.2,
   },
   {
     name: "Learning Island, Lvl 0",
     moves: "↓-↓↑↓---",
+    difficulty: 0.2,
   },
   {
     name: "Learning Island, Lvl 1",
     moves: "↓-↓↑↓-↓↑",
+    difficulty: 0.35,
   },
   {
     name: "Learning Island, Lvl 2",
     moves: "↓-↓↑↓↑↓↑",
+    difficulty: 0.5,
   },
 ];
 
+const strummingPatterns = ref([]);
 const chordsToSwitchBetween = ref([]);
+const strummingPattern = ref();
+
+function start() {
+  console.log("mounted");
+  const storedStrummingPatterns = JSON.parse(
+    localStorage.getItem("strummingPatterns")
+  );
+
+  if (storedStrummingPatterns) {
+    strummingPatterns.value = storedStrummingPatterns;
+  } else {
+    strummingPatterns.value = defaultStrummingPatternData;
+  }
+
+  strummingPattern.value =
+    strummingPatterns.value[
+      Math.floor(Math.random() * strummingPatterns.value.length)
+    ];
+  console.log("strummingPattern", strummingPattern.value);
+
+  pickTwoRandomChords();
+}
 
 function pickTwoRandomChords() {
   const randomChords = easyChords.sort(() => Math.random() - 0.5).slice(0, 2);
   chordsToSwitchBetween.value = randomChords;
 }
 
-const strummingPattern = ref(
-  strummingPatterns[Math.floor(Math.random() * strummingPatterns.length)]
-);
+function getNewStrummingPattern(difficulty) {
+  // assuming that if the user wants a harder one, this is easier, and vice-versa
+  strummingPattern.value.difficulty =
+    (strummingPattern.value.difficulty + difficulty) / 2;
 
-pickTwoRandomChords();
+  strummingPattern.value =
+    strummingPatterns.value[
+      Math.floor(Math.random() * strummingPatterns.value.length)
+    ];
+  localStorage.setItem("strummingPatterns", JSON.stringify(strummingPatterns.value));
+}
+
+start();
 </script>
 
 <template>
@@ -73,7 +110,7 @@ pickTwoRandomChords();
     <!-- make a table -->
     <!-- first row 1-2-3-4- -->
     <!-- second row: the pattern -->
-    <table class="table-auto table text-2xl my-8">
+    <table class="table-auto table text-2xl my-8" :key="strummingPattern.name">
       <tr>
         <td>1</td>
         <td>-</td>
@@ -90,6 +127,22 @@ pickTwoRandomChords();
         </td>
       </tr>
     </table>
+
+    <!-- two buttons, one for 'get harder', 'get easier' -->
+    <div class="flex gap-4">
+      <button
+        @click="getNewStrummingPattern(-1)"
+        class="bg-green-500 text-white p-2 rounded"
+      >
+        Get harder pattern
+      </button>
+      <button
+        @click="getNewStrummingPattern(1)"
+        class="bg-red-500 text-white p-2 rounded"
+      >
+        Get easier pattern
+      </button>
+    </div>
   </div>
 
   <StopButton />
