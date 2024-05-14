0daf219c9855ecbd6e81f70922f2cc468042a05e
blackbrokkoli <kastomat@gmail.com>
2024-05-14 20:13:54 +0200

    'successfully' hack in snippet mode

--- src/components/SnippetGame.vue
+++ src/components/SnippetGame.vue
@@ -4,42 +4,11 @@ import { useRouter } from "vue-router";
 
 const $router = useRouter();
 
-const currentSet = ref("Common Chords");
-
-const songSets = {
-  "Common Chords": [
-    { name: "A", frets: "2100" },
-    { name: "A#", frets: "3211" },
-    { name: "Eb", frets: "3331" },
-    { name: "E", frets: "1402" },
-    { name: "F", frets: "2010" },
-    { name: "F#", frets: "3121" },
-    { name: "Gb", frets: "3121" },
-    { name: "G", frets: "0232" },
-    { name: "G#", frets: "1343" },
-    { name: "Ab", frets: "1343" },
-  ],
-};
-
-const exercise = ref([]);
+const exercise = ref(0);
 
 function generateChordProgressionPractice() {
-  // add one random chord to the end, but keep only the last 3 chords
-  // exclude chords already in the exercise, check by frets
-  const filteredList = songSets[currentSet.value].filter(
-    (chord) => !exercise.value.some((c) => c.frets === chord.frets)
-  );
-  const randomChord =
-    filteredList[Math.floor(Math.random() * filteredList.length)];
-  exercise.value.push(randomChord);
-
-  if (exercise.value.length > 3) {
-    exercise.value.shift();
-  }
-  //   ensure 3 chords always
-  if (exercise.value.length < 3) {
-    generateChordProgressionPractice();
-  }
+  // set exercise to rng number between 0 and 39
+  exercise.value = Math.floor(Math.random() * 40);
 }
 
 const sessionStarted = ref(false);
@@ -83,18 +52,8 @@ startSession();
 <template>
   <h2 class="card-title">Switch between these two chords every 4 beats:</h2>
   <div class="bg-gray-100 p-2 flex gap-2 flex-wrap">
-    <!-- if third chord, add opacity-45 -->
-    <div
-      v-for="(chord, index) in exercise"
-      :key="index"
-      :class="index === 2 ? 'opacity-20' : ''"
-    >
-      <h3 class="font-bold">
-        <span v-if="index === 2" class="text-italic text-sm">up next:</span>
-        {{ chord.name }}
-      </h3>
-      <uke-chord :frets="chord.frets" :key="chord.name"></uke-chord>
-    </div>
+    <img  src="`/demo_snippets/legend.png`" />
+    <img :src="`/demo_snippets/${exercise}.png`" />
   </div>
 
   <div class="card-actions">
